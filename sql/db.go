5af2d094c6bd3bc7e96b66ae7967baae267c0527
foadm <foad_momtazi@hotmail.com>
2025-03-15 14:12:13 +0000

    standard interface added

--- sql/db.go
+++ sql/db.go
@@ -1,37 +1,74 @@
-package nexsql
+package main
 
 import (
 	"database/sql"
-	"fmt"
-
-	_ "github.com/microsoft/go-mssqldb"
-	_ "github.com/microsoft/go-mssqldb/integratedauth/krb5"
 )
+
+type StoredProcData struct {
+	ProcName  string
+	InputData string
+}
+
+type DBProperties struct {
+	Name       string
+	Driver     string
+	Host       string
+	Port       string
+	UserId     string
+	Password   string
+	Database   string
+	ConnString string
+}
+
+type DBInterface interface {
+	NewConnection() (*sql.DB, error)
+	CallStoredProc(c *sql.DB, f string, q string) (string, error)
+}
+
+var DBServers map[string]DBProperties = make(map[string]DBProperties)
 
 // ============================================================================
 //
 // ============================================================================
-func newConnection(driverName, connectionString string) (*sql.DB, error) {
-
-	sqlObj, connectionError := sql.Open("mssql", connectionString)
-	if connectionError != nil {
-		fmt.Println(fmt.Errorf("error opening database: %v", connectionError))
-	}
-
-	return sqlObj, connectionError
+func init() {
 }
 
-func executeProc(connection *sql.DB, procParams storedProcData) (string, error) {
-	var _rc string
-	var _err error
+// ============================================================================
+//
+// ============================================================================
+func AddDBProperty(key string, value DBProperties) {
+	DBServers[key] = value
+}
 
-	// _conn, _err := connection()
-	if _err == nil {
-		defer connection.Close()
-		_rc, _err = callStoredProc(connection, procParams.ProcName, procParams.InputData)
-		if _err == nil {
-			fmt.Printf("return data=%s\n", _rc)
-		}
+// ============================================================================
+//
+// ============================================================================
+func GetDBProperty(key string) DBProperties {
+	return DBServers[key]
+}
+
+// ============================================================================
+//
+// ============================================================================
+func (db *DBProperties) NewConnection() (*sql.DB, error) {
+	_conn, _err := sql.Open(db.Driver, db.ConnString)
+	if _err != nil {
+		logger.Printf("Unable to connect to database: %v\n", _err)
 	}
-	return _rc, _err
+	return _conn, _err
+}
+
+// ============================================================================
+//
+// ============================================================================
+func (p *DBProperties) CallStoredProc(conn *sql.DB, funcName string, query string) (string, error) {
+	var _jsonResult string
+	var _queryString string = "SELECT * FROM " + funcName + "('" + query + "')"
+	_row := conn.QueryRow(_queryString)
+	_err := _row.Scan(&_jsonResult)
+	if _err != nil {
+		logger.Printf("QueryRow failed: %v\n", _err)
+	}
+
+	return _jsonResult, _err
 }
